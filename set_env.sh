239ec49c7f5be662b52c4affd0b2476af55352f6
Man Nguyen-Minh <minhman1504@gmail.com>
2022-05-06 10:49:08 +0700

    test monolithic oke, fixed AWS_BUCKET change to bucket_name not bucket arn

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=password1
-export POSTGRES_HOST=udagram2.csixzufepzrt.us-west-2.rds.amazonaws.com
-export POSTGRES_DB=udagram
-export AWS_BUCKET=arn:aws:s3:::udaconnect
-export AWS_REGION=us-west-2
-export AWS_PROFILE=test
+export POSTGRES_USERNAME=root
+export POSTGRES_PASSWORD=root1234
+export POSTGRES_HOST=udagram-db-instance.cmxhusqjnjpo.ap-southeast-1.rds.amazonaws.com
+export POSTGRES_DB=app_database
+export AWS_BUCKET=my-670966728611-bucket #arn:aws:s3:::my-670966728611-bucket/*
+export AWS_REGION=ap-southeast-1
+export AWS_PROFILE=default
 export JWT_SECRET=testing
 export URL=http://localhost:8100
