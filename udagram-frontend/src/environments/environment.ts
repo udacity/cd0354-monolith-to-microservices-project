72cc263017a3a6edbfbcad5e0c5aa5f215e9a851
Pham Trung Tinh <tinhpt94@gmail.com>
2022-06-28 17:41:37 +0800

    Update reverse proxy url

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a5cf7b308590c45bc98e62e779e25c9d-1395600742.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
