a6a2943ada4d7aa856fbcae59452b4222c1b7c10
OllieToo16 <cheruiyotko98@gmail.com>
2022-07-08 20:16:04 +0300

    First Commit

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a2a6131e14a784d72a041a26d323caf9-73211533.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
