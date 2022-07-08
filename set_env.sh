edaabc659ea87c39819b626f0ce2a33128024fa3
TMMunyai <acumenone.tm@gmail.com>
2022-07-08 04:04:04 +0200

    1st init2

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=monolithdevdb
+export POSTGRES_PASSWORD=monolithdevdb.246
+export POSTGRES_HOST=monolith-dev-db.cuqsclk7ssqo.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=monilith-1986872-dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=Hello
+export URL=http://localhost:8100
