a5047a45c5ee326e1d905f047ace0802158ab765
Christian Osueke <osueke.christian@gmail.com>
2022-07-12 00:51:47 +0100

    chore: setup install dependecies - stop tracking set_env.sh to avoid credential leak - add mysql2 dependency for mysql db connection - add db dialet to config variables - remove omitted feed service item frim user service

--- udagram-api/udagram-feed-service/src/config/config.ts
+++ udagram-api/udagram-feed-service/src/config/config.ts
@@ -3,7 +3,7 @@ export const config = {
   'password': process.env.POSTGRES_PASSWORD,
   'database': process.env.POSTGRES_DB,
   'host': process.env.POSTGRES_HOST,
-  'dialect': 'postgres',
+  'dialect':process.env.DB_CONNECTION,
   'aws_region': process.env.AWS_REGION,
   'aws_profile': process.env.AWS_PROFILE,
   'aws_media_bucket': process.env.AWS_BUCKET,
