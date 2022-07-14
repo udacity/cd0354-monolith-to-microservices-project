d1b42f54d266992bc3362b4a5631a3a24b27b02a
unknown <tochukwuugwuanyi@gmail.com>
2022-07-14 10:45:25 +0100

    updating frontend image

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a79b8d514e48f452f9cf6d04dc783a33-1278662843.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
