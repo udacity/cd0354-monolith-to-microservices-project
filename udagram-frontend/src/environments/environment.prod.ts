17f36e8f72fc5b677f6f89bf28a1a74991398e3d
Bolatan Ibrahim <ehbraheem@gmail.com>
2022-07-19 04:11:44 +0100

    Refactor Monolith to Microservices (#1)
    * Remove secrets from source control
    
    * Refactor to microservices
    
    * Add CI config
    
    * Fix image tag
    
    * Fix dependency installation inside docker
    
    * Downgrade node
    
    * Remove platform-specific dependency
    
    * Correctlt tag images
    
    * Fix docker push command
    
    * Fix docker push command
    
    * Fix docker script
    
    * Add bash
    
    * Simple travis
    
    * Change docker node version
    
    * Add env to docker
    
    * Change base image
    
    * Reduce container resource usage
    
    * Debugging
    
    * Remove debug logging
    
    * Fix Nginx config
    
    * Using reverseproxy's cluster IP
    
    * Update frontend url
    
    * Add k8 configs
    
    * Add screenshots
    
    * Remove sensitive info

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a8e568150ec954f94b797cb91100ebbd-590019785.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
