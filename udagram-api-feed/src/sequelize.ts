9472636aa09026a4eceded2799fcbd66c0700651
Ololade Olanrewaju <Oolarenwaju@wragbysolutions.com>
2022-07-24 22:07:12 +0100

    Microservices

--- udagram-api-feed/src/sequelize.ts
+++ udagram-api-feed/src/sequelize.ts
@@ -1,4 +1,5 @@
 import {Sequelize} from 'sequelize-typescript';
+import { Dialect } from 'sequelize';
 import {config} from './config/config';
 
 
@@ -8,6 +9,6 @@ export const sequelize = new Sequelize({
   'database': config.database,
   'host': config.host,
 
-  'dialect': config.dialect,
+  'dialect': config.dialect as Dialect,
   'storage': ':memory:',
 });
