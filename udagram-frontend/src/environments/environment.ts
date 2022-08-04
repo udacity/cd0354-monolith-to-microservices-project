a2215ac5cbd7cd71c2ac9713f0b0676f68ecf341
Daniel Otulagun <daniel.otulagun@pioneeringprogrammers.com>
2022-08-05 00:30:55 +0100

    push

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  //apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://aba3af3cf0593416ea2801bf7028298b-651213667.us-west-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
