faeabab53890f9d2e43846e8aa4160a6f0752819
haitham911 <haitham911eg@gmail.com>
2022-08-13 00:51:14 +0200

    taml service

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'https://e7669ead6cb0bce007df774355365942.gr7.us-east-2.eks.amazonaws.com/api/v0'
 };
 
 /*
