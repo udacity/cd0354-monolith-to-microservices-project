19bed89d05af7b6650a32be04581e0e9b2b701a4
dims3753 <dimeji3753@gmail.com>
2022-08-29 06:39:14 +0100

    Refactored Monolith into Microservices

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,12 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
+export POSTGRES_USERNAME=dimicrodb
+export POSTGRES_PASSWORD=dimicrodb
+export POSTGRES_HOST=dimicrodb.ce6oyzabkldt.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+export AWS_BUCKET=dimicro
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
 export URL=http://localhost:8100
+
