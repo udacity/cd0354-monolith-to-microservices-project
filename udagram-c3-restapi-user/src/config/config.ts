bf699eefda4869cb7e4e4917108554cdaf26226a
austinokari1998 <austinokari@gmail.com>
2022-08-30 17:15:16 +0300

    my commit

---
+++ udagram-c3-restapi-user/src/config/config.ts
@@ -0,0 +1,24 @@
+export const config = {
+  "dev": {
+    "username": process.env.POSTGRES_USERNAME,
+    "password": process.env.POSTGRES_PASSWORD,
+    "database": process.env.POSTGRES_DB,
+    "host": process.env.POSTGRES_HOST,
+    "dialect": "postgres",
+    "aws_region": process.env.AWS_REGION,
+    "aws_profile": "default",
+    "aws_media_bucket": process.env.AWS_BUCKET,
+    "url": process.env.URL
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "udagram_prod",
+    "host": "",
+    "dialect": "postgres"
+  },
+  "jwt": {
+    "secret": process.env.JWT_SECRET
+  }
+
+}
