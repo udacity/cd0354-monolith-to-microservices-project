094549cca6db83e8506f8f09a7e5942313d83801
austinokari1998 <austinokari@gmail.com>
2022-09-03 09:46:38 +0300

    project review

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://ac2be89c2cb1840b8970681f3fa27ba8-1494808883.us-east-1.elb.amazonaws.com:8080/v0'
+  apiHost: 'http://localhost:8080/api/v0'
 };
 
 /*
