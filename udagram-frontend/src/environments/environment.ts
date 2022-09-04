8a81dac2c6987ae539a3e32115c1632628730fa2
bShopelu <boluwatifeshopelu11@gmail.com>
2022-09-04 19:06:24 +0100

    updated

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ad820f8f0dbb247f392986e001fddafa-1087562653.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
