c03b0ca5694559e44e10fde491cc218e3db3070e
VanTruongUIT <tvtruong.uit@gmail.com>
2022-09-07 23:54:52 +0700

    wip part2 project3

---
+++ udagram-api-feed/src/config/config.ts
@@ -0,0 +1,14 @@
+export const config = {
+  'username': 'postgres',
+  'password': 'postgresAbc111',
+  'database': 'postgres',
+  'host': 'postgres.cohefwz5riff.us-east-1.rds.amazonaws.com',
+  'dialect': 'postgres',
+  'aws_region': 'us-east-1',
+  'aws_profile': 'serverless-admin',
+  'aws_media_bucket': 'c2-udacity',
+  'url': 'http://localhost:8100',
+  'jwt': {
+    'secret': 'testing',
+  },
+};
--- udagram-api/src/config/config.ts
+++ udagram-api/src/config/config.ts
@@ -1,14 +1,14 @@
 export const config = {
-  'username': process.env.POSTGRES_USERNAME,
-  'password': process.env.POSTGRES_PASSWORD,
-  'database': process.env.POSTGRES_DB,
-  'host': process.env.POSTGRES_HOST,
+  'username': 'postgres',
+  'password': 'postgresAbc111',
+  'database': 'postgres',
+  'host': 'postgres.cohefwz5riff.us-east-1.rds.amazonaws.com',
   'dialect': 'postgres',
-  'aws_region': process.env.AWS_REGION,
-  'aws_profile': process.env.AWS_PROFILE,
-  'aws_media_bucket': process.env.AWS_BUCKET,
-  'url': process.env.URL,
+  'aws_region': 'us-east-1',
+  'aws_profile': 'serverless-admin',
+  'aws_media_bucket': 'c2-udacity',
+  'url': 'http://localhost:8100',
   'jwt': {
-    'secret': process.env.JWT_SECRET,
+    'secret': 'testing',
   },
 };
