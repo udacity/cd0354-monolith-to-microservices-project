caa3ea11398bba8e7d170c328b4c3a6436223584
abanoubwagih <abanoubwagih@gmail.com>
2022-09-09 07:09:55 +0200

    in medi

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
-export AWS_REGION=us-east-1
-export AWS_PROFILE=default
-export JWT_SECRET=testing
-export URL=http://localhost:8100
+setx POSTGRES_USERNAME "Abanoub" /M
+setx POSTGRES_PASSWORD "Abanoub+1" /M
+setx POSTGRES_HOST "rds-project3.csfadcykr6xx.us-east-1.rds.amazonaws.com" /M
+setx POSTGRES_DB "postgres" /M
+setx AWS_BUCKET "s3-bucket-with-my-money" /M
+setx AWS_REGION "us-east-1" /M
+setx AWS_PROFILE "default" /M
+setx JWT_SECRET "testing" /M
+setx URL "http://localhost:8100" /M
