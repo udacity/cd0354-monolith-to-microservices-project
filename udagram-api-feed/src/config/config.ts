032b6fa584f55f47e6e1e622f37851180016de77
Misge <tesfaymisgana@gmail.com>
2022-09-15 20:43:11 +0300

    microservice_application

---
+++ udagram-api-feed/src/config/config.ts
@@ -0,0 +1,14 @@
+export const config = {
+  'username': 'postgres',
+  'password': 'postgres',
+  'database': 'postgres',
+  'host': 'database-1.cpz8hh4xihsr.us-east-1.rds.amazonaws.com',
+  'dialect': 'postgres',
+  'aws_region': 'us-east-1',
+  'aws_profile': 'default',
+  'aws_media_bucket': 'tesr-7888585581-dev',
+  'url': 'https://localhost:8100',
+  'jwt': {
+    'secret': 'test',
+  },
+};
