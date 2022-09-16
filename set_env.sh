842cf29ba39d7fc420b23476fcee417711d48fd2
abdoulhakamtaba <abdoulhakamtaba@gmail.com>
2022-09-16 15:29:28 +0100

    No credentials on repo

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
-export AWS_REGION=us-east-1
-export AWS_PROFILE=default
-export JWT_SECRET=testing
-export URL=http://localhost:8100
+setx POSTGRES_USERNAME=tabaudacitydev
+setx POSTGRES_PASSWORD=tabaudacitydev
+setx POSTGRES_HOST=tabaudacitydev.cgzaqaevp08b.us-east-1.rds.amazonaws.com
+setx POSTGRES_DB=postgres
+setx AWS_BUCKET=taba-udacity-dev-s3
+setx AWS_REGION=us-east-1
+setx AWS_PROFILE=default
+setx JWT_SECRET=testing
+setx URL=http://localhost:8100
