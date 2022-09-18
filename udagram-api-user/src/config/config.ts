d263d3e86592ab318dfc599cbd4a0670f6791b40
yaasin12 <yassin.j@live.com>
2022-09-18 19:23:07 +0400

    Main API divided to 2 APIS and Docker config/Nginx config added

---
+++ udagram-api-user/src/config/config.ts
@@ -0,0 +1,14 @@
+export const config = {
+  'username': process.env.POSTGRES_USERNAME,
+  'password': process.env.POSTGRES_PASSWORD,
+  'database': process.env.POSTGRES_DB,
+  'host': process.env.POSTGRES_HOST,
+  'dialect': 'postgres',
+  'aws_region': process.env.AWS_REGION,
+  'aws_profile': process.env.AWS_PROFILE,
+  'aws_media_bucket': process.env.AWS_BUCKET,
+  'url': process.env.URL,
+  'jwt': {
+    'secret': process.env.JWT_SECRET,
+  },
+};
