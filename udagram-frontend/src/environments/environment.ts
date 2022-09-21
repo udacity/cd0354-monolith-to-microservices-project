1834715aaf21aa84c5bef9e29ffca530cfe19a5f
SaberGuirat <s.guirat@digital-expert.io>
2022-09-21 02:09:37 +0200

    fix: front-end url

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a3861bcefec6046cb9a6769af72dcf9e-1850535507.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
