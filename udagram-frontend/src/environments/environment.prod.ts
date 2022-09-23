d395795d7c736337851c0454ac5076045cfab1df
Jashon Osala <jashonosala@gmail.com>
2022-09-23 09:49:16 +0300

    Config

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ac8ed5e46ef2c40128f673986a3c51dc-1882881608.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
