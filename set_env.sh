711e033c81d7fe689bf8ac9abcda3cadfac7423e
Ahmed-Yulcom <93929978+Ahmed-Yulcom@users.noreply.github.com>
2022-09-25 15:11:21 +0000

    structure

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,12 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+export POSTGRES_USERNAME=udagram
+export POSTGRES_PASSWORD=password
+export POSTGRES_HOST=udagram.cdghg7vti4un.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=udagram
+export AWS_BUCKET=arn:aws:s3:::s3-udgram-monolothic-to-microservice-475659817827
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
 export URL=http://localhost:8100
+# S3 s3-udgram-monolothic-to-microservice-475659817827
