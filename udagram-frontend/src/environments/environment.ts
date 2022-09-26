23ee07d5b12ac79dc730181d73452967f7b2304d
TatchumNono <Tatchumnono@gmail.com>
2022-09-26 22:10:26 +0100

    added reverseproxy link to frontend env

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'a786a7529ddad44bba7a4799dbbf12be-216391315.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
