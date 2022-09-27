bc41d673878466bca4c48ce2d2dd2211431f0a1e
SanMedElhadi <fm_SANNEF@esi.dz>
2022-09-27 19:23:22 +0100

    State of Project in 27/09/2022

---
+++ set_env_windows.sh
@@ -0,0 +1,12 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+set POSTGRES_USERNAME=postgres
+set POSTGRES_PASSWORD=postgres
+set POSTGRES_HOST=database-1.cxn118ypvppv.us-east-1.rds.amazonaws.com
+set POSTGRES_DB=postgres
+set AWS_BUCKET= mono-udagram-702960059720-dev
+set AWS_REGION=us-east-1
+set AWS_PROFILE=default
+set JWT_SECRET=testing
+set URL=http://localhost:8100
+set PORT=8080
