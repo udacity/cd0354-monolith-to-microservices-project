0e1dbc93c8dd5dbb3cfe649dffc68c019dd76071
unknown <uwemabasifrekelive@gmail.com>
2022-09-30 12:21:00 +0100

    my first commit. separated into feed and user, added frontend and reverse proxy. edited dependencies in package.json file accordingly, added the yaml files for building docker images and containers, set and hid my environment variables

---
+++ udagram-api-user/src/server.ts
@@ -0,0 +1,52 @@
+require('dotenv').config()
+
+import cors from 'cors';
+import express from 'express';
+import {sequelize} from './sequelize';
+
+import {IndexRouter} from './controllers/v0/index.router';
+
+import bodyParser from 'body-parser';
+import {config} from './config/config';
+import {V0_USER_MODELS} from './controllers/v0/model.index';
+
+
+(async () => {
+  await sequelize.addModels(V0_USER_MODELS);
+
+  console.debug("Initialize database connection...");
+  await sequelize.sync();
+
+  const app = express();
+  const port = process.env.PORT || 8080;
+
+  app.use(bodyParser.json());
+
+  // We set the CORS origin to * so that we don't need to
+  // worry about the complexities of CORS this lesson. It's
+  // something that will be covered in the next course.
+  app.use(cors({
+    allowedHeaders: [
+      'Origin', 'X-Requested-With',
+      'Content-Type', 'Accept',
+      'X-Access-Token', 'Authorization',
+    ],
+    methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+    preflightContinue: true,
+    origin: '*',
+  }));
+
+  app.use('/api/v0/', IndexRouter);
+
+  // Root URI call
+  app.get( '/', async ( req: any, res: { send: (arg0: string) => void; } ) => {
+    res.send( '/api/v0/' );
+  } );
+
+
+  // Start the Server
+  app.listen( port, () => {
+    console.log( `server running ${config.url}` );
+    console.log( `press CTRL+C to stop server` );
+  } );
+})();
