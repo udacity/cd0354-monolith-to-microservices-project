8e146a9b206c5b9e27ef7e0b34fa3fe7dea4bed9
ndzib <brunoserkwi@gmail.com>
2022-10-02 21:56:54 +0100

    used hosted proxy

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a6bca5ee925464415a58d0136d5fbf68-1742248602.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
