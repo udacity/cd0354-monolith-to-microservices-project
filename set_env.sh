e1ab08c5913c6d80142efd730fad844b8ed727b5
sharonibejih <sharonibejih@gmail.com>
2022-10-05 05:15:01 +0100

    refactored to microservices and dockerized

--- set_env.sh
+++ set_env.sh
@@ -1,10 +1,10 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+export POSTGRES_PASSWORD=admin1234
+export POSTGRES_HOST=udagram-instance.cbsax0axnlxt.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=udagramdb # database name
+export AWS_BUCKET=udagram-user-files
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
