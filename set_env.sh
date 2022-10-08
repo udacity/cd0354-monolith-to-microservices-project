794b5ec8a1ce57846059486e77c11e587c9ce2f8
Xolane-Black <xolanemnisi@gmail.com>
2022-10-08 13:50:08 +0200

    Microservices app in local mode

--- set_env.sh
+++ set_env.sh
@@ -1,10 +1,10 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+export POSTGRES_USERNAME=k8database
+export POSTGRES_PASSWORD=Ab93j#mx
+export POSTGRES_HOST=k8database.czpasiikaulg.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=k8database
+export AWS_BUCKET=xolane-k8-bucket
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
