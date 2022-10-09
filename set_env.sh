8a5801cdd6e6c00ebb76022ed9888531ba3b10bd
Godfred Afari Mintah <godfredafarimintah@Godfreds-MacBook-Air.local>
2022-10-09 10:34:43 +0000

    Seperated monolith to microservices and added docker-compose yaml file to build concurrent images

--- set_env.sh
+++ set_env.sh
@@ -1,10 +1,10 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+export POSTGRES_USERNAME=donnis
+export POSTGRES_PASSWORD=donnis25
+export POSTGRES_HOST=udm.cr64e6hvu4kx.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=udagramm
+export AWS_BUCKET=udagrambucket2005
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
