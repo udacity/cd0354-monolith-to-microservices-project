bd57fc4d9112925e114d34917a56a85239c40ffd
kouna <kouna45@outlook.com>
2022-10-09 22:10:49 +0000

    Refactor docker_push file

--- docker_push.sh
+++ docker_push.sh
@@ -2,15 +2,15 @@
 
 docker-compose -f docker-compose-build.yaml build
 
-docker tag reverseproxy "${DOCKER_USERNAME}/reverseproxy:v1.2"
-docker tag udagram-api-user "${DOCKER_USERNAME}/udagram-api-user:v1.1"
-docker tag udagram-api-feed "${DOCKER_USERNAME}/udagram-api-feed:v1.1"
-docker tag udagram-frontend:local "${DOCKER_USERNAME}/udagram-frontend:v1.2"
+docker tag reverseproxy "kouna77/reverseproxy:v1.2"
+docker tag udagram-api-user "kouna77/udagram-api-user:v1.1"
+docker tag udagram-api-feed "kouna77/udagram-api-feed:v1.1"
+docker tag udagram-frontend:local "kouna77/udagram-frontend:v1.2"
 
-echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
+echo F@llou77 | docker login --username kouna77 --password-stdin
 
-docker push "${DOCKER_USERNAME}/reverseproxy:v1.2" &
-docker push "${DOCKER_USERNAME}/udagram-api-user:v1.1" &
-docker push "${DOCKER_USERNAME}/udagram-api-feed:v1.1" &
-docker push "${DOCKER_USERNAME}/udagram-frontend:v1.2" &
+docker push "kouna77/reverseproxy:v1.2" &
+docker push "kouna77/udagram-api-user:v1.1" &
+docker push "kouna77/udagram-api-feed:v1.1" &
+docker push "kouna77/udagram-frontend:v1.2" &
 wait
