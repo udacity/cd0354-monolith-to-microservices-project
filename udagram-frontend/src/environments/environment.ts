9a9e0e06df64e3529729b2c766c9b38338517900
0xceejay <0xceejay@gmail.com>
2022-10-19 10:36:46 +0100

    Correct

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   appName: 'Udagram',
   apiHost: 'http://localhost:8080/api/v0'
-
+};
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
