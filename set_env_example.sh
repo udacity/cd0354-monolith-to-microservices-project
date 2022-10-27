f067884d97662e6592fb013553ea073b061b2633
alexxsanya <alexxsanya@gmail.com>
2022-10-28 00:58:02 +0300

    break app in microservice

---
+++ set_env_example.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=udagram-db
+export AWS_BUCKET=arn:aws:s3:::udagram-163007148323-dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
