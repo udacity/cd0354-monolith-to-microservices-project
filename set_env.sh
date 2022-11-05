113abf75ef7bba6cbfd776b7aa27ee9bc7303d17
Duongg <ducduong2201@gmail.com>
2022-11-05 22:21:15 +0700

    breaking monolith app to microservice

--- set_env.sh
+++ set_env.sh
@@ -1,8 +1,8 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
+export POSTGRES_USERNAnpmME=postgres
+export POSTGRES_PASSWORD=admin
+export POSTGRES_HOST=localhost
 export POSTGRES_DB=postgres
 export AWS_BUCKET=arn:aws:s3:::mybucket1200798
 export AWS_REGION=us-east-1
