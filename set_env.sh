7de0624df103cc4d01dd2736ed2540cfef3261fc
Karim Fadel <karim.fadel@sap.com>
2022-11-07 20:31:57 +0200

    convert monolith to microservices
    update the environment variable
    convert monolith to microservices
    update the typescript dependency version in user project
    add docker files
    add docker-compose file
    create a reverse proxy

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_HOST=udagram-db-636624855516-dev.c3xiypftfmft.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+export AWS_BUCKET=udagram-636624855516-dev
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
-export JWT_SECRET=testing
+export JWT_SECRET=udagramJWTSecret
 export URL=http://localhost:8100
