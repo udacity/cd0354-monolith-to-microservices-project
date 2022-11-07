7a37c71b8718c03d2679cfa6c98aeaa0a16813c0
Duongg <ducduong2201@gmail.com>
2022-11-07 23:20:24 +0700

    set up env to post image

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,13 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAnpmME=postgres
-export POSTGRES_PASSWORD=admin
-export POSTGRES_HOST=localhost
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=mypassword
+export POSTGRES_HOST=postgres.cfwjyx5clqlq.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+export AWS_BUCKET=microservice-3-bucket
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
 export URL=http://localhost:8100
+
+
--- udagram-api-user/src/sequelize.ts
+++ udagram-api-user/src/sequelize.ts
@@ -1,7 +1,8 @@
 import {Sequelize} from 'sequelize-typescript';
 import {config} from './config/config';
 
-
+console.log(JSON.stringify(config))
+console.log(JSON.stringify(process.env))
 export const sequelize = new Sequelize({
   'username': config.username,
   'password': config.password,
