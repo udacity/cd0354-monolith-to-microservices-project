a0be1c301d1c461f3c2bb7ba616da26d9537ed4f
Zacchaeus Ayanniran <zacchdipo@gmail.com>
2022-11-27 16:24:14 +0000

    Added changes

--- set_env.sh
+++ set_env.sh
@@ -8,4 +8,4 @@ export AWS_BUCKET=zac-project-dev
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
-export URL=http://localhost:8080
+export URL=http://localhost:8100
