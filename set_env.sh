5e18d1e49eaff0ccde35b592950012bd20121fdb
Ayomide26 <ayomidefadare26@gmail.com>
2022-11-30 10:40:33 +0100

    project3

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,12 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=hellotry
-export POSTGRES_PASSWORD=tryhello
-export POSTGRES_HOST=cdrs.cdxyixs049km.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=postgres
-export AWS_BUCKET=trymm
-export AWS_REGION=us-east-1
-export AWS_PROFILE=default
-export JWT_SECRET=testing
+export POSTGRES_USERNAME="vicky"
+export POSTGRES_PASSWORD="lastchance"
+export POSTGRES_HOST="databaselast.cjzscjnt37c9.us-east-1.rds.amazonaws.com"
+export POSTGRES_DB="postgres"
+export AWS_BUCKET="lastchanceproject"
+export AWS_REGION="us-east-1"
+export AWS_PROFILE="default"
+export JWT_SECRET="testing"
 export URL=http://localhost:8100
+
