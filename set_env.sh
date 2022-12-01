a20ebd65fa909de4e7278023dbd2b567bf3b0697
Quy Nguyen <mvpvn.official@gmail.com>
2022-12-01 17:37:59 +0700

    update variables/parameters

--- set_env.sh
+++ set_env.sh
@@ -2,10 +2,10 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
+export POSTGRES_HOST=capstone-rds-dbinstance-b3gxtx17mi0q.czrpqvje9hpa.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+export AWS_BUCKET=mybucket058763079521
 export AWS_REGION=us-east-1
-export AWS_PROFILE=default
+export AWS_PROFILE=udacity
 export JWT_SECRET=testing
 export URL=http://localhost:8100
