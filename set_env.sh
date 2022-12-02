344470031e287dd04a5e5dd502b2d688c6e6c809
tien.nguyendong <dongtien.nguyen93@gmail.com>
2022-12-02 10:02:46 +0700

    Refactor to microservice

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
+export POSTGRES_PASSWORD=Vietnam@1234
+export POSTGRES_HOST=localhost
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+export AWS_BUCKET=monolith-2-microservices-udacity
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
-export JWT_SECRET=testing
+export JWT_SECRET=hello
 export URL=http://localhost:8100
