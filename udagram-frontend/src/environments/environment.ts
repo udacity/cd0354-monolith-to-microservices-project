0832ea2f5ec0711b7fd2d83a7e2e0468c1ed4988
Quy Nguyen <mvpvn.official@gmail.com>
2022-12-30 17:00:28 +0700

    update tag version

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a8718a0f8d2e8431090a60d0c274a057-1547620139.ap-southeast-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
