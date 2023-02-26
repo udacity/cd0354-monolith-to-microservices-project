7159840ace9632138c75071a7d3a7502c0f039b6
zmobarez <zmoba4@gmail.com>
2023-02-26 21:09:34 +0200

    moify

--- set_env.sh
+++ set_env.sh
@@ -4,9 +4,8 @@ export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=mypassword
 export POSTGRES_HOST=database-1.cmfeeistpgqu.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::test-765747930858-dev
+export AWS_BUCKET=test-765747930858-dev
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
 export URL=http://localhost:8100
-*set_env.sh
