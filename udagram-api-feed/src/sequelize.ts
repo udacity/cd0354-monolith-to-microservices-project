05bfc0d020ab3d84c6d19eea430c65fbf75d3427
ozieblo <ozieblo.michal@gmail.com>
2023-04-04 22:25:00 +0200

    travis ci

--- udagram-api-feed/src/sequelize.ts
+++ udagram-api-feed/src/sequelize.ts
@@ -1,13 +1,25 @@
-import {Sequelize} from 'sequelize-typescript';
-import {config} from './config/config';
+// import {Sequelize} from 'sequelize-typescript';
+// import {config} from './config/config';
 
 
-export const sequelize = new Sequelize({
-  'username': config.username,
-  'password': config.password,
-  'database': config.database,
-  'host': config.host,
+// export const sequelize = new Sequelize({
+//   'username': config.username,
+//   'password': config.password,
+//   'database': config.database,
+//   'host': config.host,
 
-  'dialect': config.dialect,
-  'storage': ':memory:',
-});
+//   'dialect': config.dialect,
+//   'storage': ':memory:',
+// });
+
+
+import { Sequelize, ISequelizeConfig } from 'sequelize-typescript';
+import { config } from './config/config';
+export const sequelize = new Sequelize(
+{ username: config.username,
+password: config.password,
+database: config.database,
+host: config.host,
+dialect: config.dialect,
+storage: ':memory:'} as ISequelizeConfig
+);
