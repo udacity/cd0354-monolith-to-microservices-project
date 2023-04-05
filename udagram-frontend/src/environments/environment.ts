2eb55de24db5eac51e4d75eb083a36f11652fee2
ozieblo <ozieblo.michal@gmail.com>
2023-04-05 15:54:13 +0200

    Container Orchestration with Kubernetes

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ace607edff3e7406896af06a6343ed68-970045066.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
