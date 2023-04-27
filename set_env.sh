cf1b41baebdd3e1e7e5b467e347b1a43323ab32c
Phong Tran <YuTran@Yus-iMac.local>
2023-04-27 20:06:23 +0700

    Project 03

--- set_env.sh
+++ set_env.sh
@@ -1,10 +1,10 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+export POSTGRES_USERNAME=phongtq2
+export POSTGRES_PASSWORD=12345678
+export POSTGRES_HOST=phongtq2.culzlpruhdo7.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=phongtq2
+export AWS_BUCKET=myawsbucket-phongtq2
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
