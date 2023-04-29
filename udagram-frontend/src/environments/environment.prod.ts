065c694a457aa447e1990cbcc751838ec2329ff2
Tai Tran <transytai12a1@gmail.com>
2023-04-29 08:41:11 +0700

    Update backend reverseproxy

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://ad46d5500999642e0b21b25b3df50ad2-1457405120.us-east-1.elb.amazonaws.com:8080/api/v0'
+  apiHost: 'http://a067877fc499a44cc807b38d77bb1a56-1129297167.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
