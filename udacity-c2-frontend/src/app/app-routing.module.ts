f1970638f32669dbe47f0d3a3198f67bc23de415
Trieunv <macos@macOSs-MacBook-Pro.local>
2023-06-03 23:50:42 +0700

    try node 14

---
+++ udacity-c2-frontend/src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules, relativeLinkResolution: 'legacy' })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
