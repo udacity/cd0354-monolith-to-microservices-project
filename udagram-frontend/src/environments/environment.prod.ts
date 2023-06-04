c72fcb124e0072767ce4abbd5e1260563757795e
Trieunv <macos@macOSs-MacBook-Pro.local>
2023-06-04 15:12:45 +0700

    final

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'aba4c4097bebb43eb8c314983caa2e37-1627929286.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
