b9f3a8410207281b053b76e790b0eb1c1fe58542
vuthuhatk4 <vuthuhatk4@gmail.com>
2023-09-16 14:48:51 +0700

    prj3 - proxy fail

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
+export POSTGRES_USERNAME=havtdatabase
+export POSTGRES_PASSWORD=havtdatabase
+export POSTGRES_HOST=havtdatabase.cftwsdxdaq6p.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+export AWS_BUCKET=arn:aws:s3:::havt-bucket
 export AWS_REGION=us-east-1
-export AWS_PROFILE=default
+export AWS_PROFILE=hatk4
 export JWT_SECRET=testing
 export URL=http://localhost:8100
---
+++ udagram-api-feed/src/aws.ts
@@ -0,0 +1,35 @@
+import AWS = require('aws-sdk');
+import {config} from './config/config';
+
+
+// Configure AWS
+const credentials = new AWS.SharedIniFileCredentials({profile: config.aws_profile});
+AWS.config.credentials = credentials;
+
+export const s3 = new AWS.S3({
+  signatureVersion: 'v4',
+  region: config.aws_region,
+  params: {Bucket: config.aws_media_bucket},
+});
+
+// Generates an AWS signed URL for retrieving objects
+export function getGetSignedUrl( key: string ): string {
+  const signedUrlExpireSeconds = 60 * 5;
+
+  return s3.getSignedUrl('getObject', {
+    Bucket: config.aws_media_bucket,
+    Key: key,
+    Expires: signedUrlExpireSeconds,
+  });
+}
+
+// Generates an AWS signed URL for uploading objects
+export function getPutSignedUrl( key: string ): string {
+  const signedUrlExpireSeconds = 60 * 5;
+
+  return s3.getSignedUrl('putObject', {
+    Bucket: config.aws_media_bucket,
+    Key: key,
+    Expires: signedUrlExpireSeconds,
+  });
+}
