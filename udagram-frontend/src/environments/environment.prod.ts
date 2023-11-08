461fbb046a78873c3dfef33a713edf17e7b02383
Tan Nguyen <ngtrantan99@gmail.com>
2023-11-09 00:48:05 +0700

    upodate

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://a2e6d91c9657442d6ac5da5372f6b354-466475747.us-east-1.elb.amazonaws.com:8080/api/v0'
+  apiHost: 'http://localhost:8080/api/v0'
 };
 
 /*
