f600c3f1831f919d9421945c93f52dac6581320a
minhducvt293 <39126122+minhducvt293@users.noreply.github.com>
2023-11-08 18:14:13 +0700

    test

--- docker_push.sh
+++ docker_push.sh
@@ -3,5 +3,5 @@
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push minhducvt293/udagram-api-feed:v1
 docker push minhducvt293/udagram-api-user:v1
-docker push minhducvt293/udagram-api-frontend:v1
-docker push minhducvt293/udagram-api-reverseproxy:v1
+docker push minhducvt293/udagram-frontend:v1
+docker push minhducvt293/reverseproxy:v1
