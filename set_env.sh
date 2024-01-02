249cfc03b76fbe12e2f309627cd2ae325150e33a
Andrej Kovacic <andrejkovacic123@gmail.com>
2024-01-02 16:12:52 +0100

    Create user and feed microservices

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=postgres
-export AWS_BUCKET=mybucket1200798
-export AWS_REGION=us-east-1
-export AWS_PROFILE=default
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_HOST=localhost
+export POSTGRES_DB=udagram-db
+export AWS_BUCKET=udagram-images
+export AWS_REGION=eu-central-1
+export AWS_PROFILE=udagram
 export JWT_SECRET=testing
 export URL=http://localhost:8100
