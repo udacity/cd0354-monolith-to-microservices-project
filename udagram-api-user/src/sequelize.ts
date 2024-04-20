a120f17ad2abc97905575ccd0f540ec93d284800
dattm283 <dattm283@gmail.com>
2024-04-20 23:45:07 +0700

    Run the project locally in a multi-container environment

---
+++ udagram-api-user/src/sequelize.ts
@@ -0,0 +1,12 @@
+import {Sequelize} from 'sequelize-typescript';
+import {config} from './config/config';
+
+
+export const sequelize = new Sequelize({
+  'username': config.username,
+  'password': config.password,
+  'database': config.database,
+  'host': config.host,
+  'dialect': config.dialect,
+  'storage': ':memory:',
+});
