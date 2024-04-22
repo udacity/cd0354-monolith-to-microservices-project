f895733359d1992d46c9ce13d265b7b93888cd88
inc511 <92462075+inc511@users.noreply.github.com>
2024-04-22 23:13:19 +0700

    bug fixes compile

--- set_env.sh
+++ set_env.sh
@@ -1,8 +1,8 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
+export POSTGRES_PASSWORD=thanhnd61
+export POSTGRES_HOST=database-1.cc75ytoh4oag.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
 export AWS_BUCKET=arn:aws:s3:::mybucket1200798
 export AWS_REGION=us-east-1
