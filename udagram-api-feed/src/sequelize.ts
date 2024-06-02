c433db9cd7aa0bd3432484bac76be737259e7ead
Cuong <ngngocuong@gmail.com>
2024-06-02 22:49:04 +0700

    break source for

---
+++ udagram-api-feed/src/sequelize.ts
@@ -0,0 +1,18 @@
+import {Sequelize} from 'sequelize-typescript';
+import {config} from './config/config';
+
+
+export const sequelize = new Sequelize({
+  'username': config.username,
+  'password': config.password,
+  'database': config.database,
+  'host': config.host,
+
+  'dialect': config.dialect,
+  'storage': ':memory:',
+  'dialectOptions': {
+    ssl: {
+      rejectUnauthorized: false, // Trust the self-signed certificate
+    }
+  }
+});
