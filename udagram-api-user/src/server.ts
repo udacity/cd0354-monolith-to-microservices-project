0941b3bec26a610214da3362a1b43a537af3b87b
anhnh-n299 <anhnhn299@gmail.com>
2024-08-07 14:44:08 +0700

    trigger

--- udagram-api-user/src/server.ts
+++ udagram-api-user/src/server.ts
@@ -44,7 +44,7 @@ import {V0_USER_MODELS} from './controllers/v0/model.index';
 
   // Start the Server
   app.listen( port, () => {
-    console.log( `server running ${config.url}` );
+    console.log( `server is running ${config.url}` );
     console.log( `press CTRL+C to stop server` );
   } );
 })();
