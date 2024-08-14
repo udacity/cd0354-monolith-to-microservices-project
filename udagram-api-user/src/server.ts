f73735b079e115b4242eb516464994b07c232ae1
vanchung1995 <vanchung1995@gmail.com>
2024-08-14 23:13:44 +0700

    update docker

--- udagram-api-user/src/server.ts
+++ udagram-api-user/src/server.ts
@@ -16,7 +16,7 @@ import {V0_USER_MODELS} from './controllers/v0/model.index';
   await sequelize.sync();
 
   const app = express();
-  const port = process.env.PORT || 8080;
+  const port = process.env.PORT || 8010;
 
   app.use(bodyParser.json());
 
