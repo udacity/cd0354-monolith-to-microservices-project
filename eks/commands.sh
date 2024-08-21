f26d045b75cf619e296806cf86f5bb147ea66edf
Vũ Văn Chung <vanchung1995@gmail.com>
2024-08-21 10:07:22 +0700

    Create commands.sh [ci skip]

---
+++ eks/commands.sh
@@ -0,0 +1,44 @@
+# describe pods
+kubectl get pods
+kubectl describe pod [pod-name]
+kubectl logs [pod-name]
+kubectl exec --stdin --tty [pod-name] -- [/bin/bash | sh]
+
+# describe services
+kubectl get services
+kubectl describe services
+
+# describe hpa
+kubectl describe hpa
+
+# describe deployments
+kubectl get deployments
+
+# apply all deployments and services
+kubectl apply -f backend-feed-deployment.yaml 
+kubectl apply -f backend-user-deployment.yaml
+kubectl apply -f backend-feed-service.yaml 
+kubectl apply -f backend-user-service.yaml 
+
+kubectl apply -f reverse-proxy-deployment.yaml 
+kubectl apply -f frontend-deployment.yaml 
+
+# expose services to external internet
+kubectl expose deployment frontend --type=LoadBalancer --name=frontend
+kubectl expose deployment reverse-proxy --type=LoadBalancer --name=reverse-proxy
+
+# mount file to configmap
+kubectl create configmap nginx-router-config --from-file=nginx.conf
+
+# mount file to secret
+kubectl create secret generic aws-secret --from-file=$HOME/.aws/credentials
+
+volumes:
+    - name: aws-credentials
+    secret:
+        secretName: aws-secret 
+
+volumeMounts:
+    - name: aws-credentials
+    mountPath: /root/.aws/credentials
+    subPath: credentials
