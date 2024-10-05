31f3497785a23ea851b551c34b72d7cd83eade11
NguyenMinhQuang <mquang146@gmail.com>
2024-10-05 23:30:03 +0700

    update config

--- set_env.sh
+++ set_env.sh
@@ -2,7 +2,7 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=postgres
-export POSTGRES_HOST=database-udagram.csorpqanefje.us-east-1.rds.amazonaws.com
+export POSTGRES_HOST=postgres.codctohgfvrc.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
 export AWS_BUCKET=mybucket-project03
 export AWS_REGION=us-east-1
--- udagram-api-feed/src/aws.ts
+++ udagram-api-feed/src/aws.ts
@@ -1,7 +1,7 @@
 import AWS = require('aws-sdk');
 import {config} from './config/config';
 
-
+console.log("=========config===========", JSON.stringify(config));
 // Configure AWS
 const credentials = new AWS.SharedIniFileCredentials({profile: config.aws_profile});
 AWS.config.credentials = credentials;
